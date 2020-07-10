fa6da945244faddceb5eb7e1d605351bad83ef12
Cindy Smith <cmsmith43152@gmail.com>
2020-07-10 11:50:54 -0400

    resubmitting assessment

--- main.js
+++ main.js
@@ -1,24 +1,25 @@
+
 const bodyElement = document.querySelector('body')
 const mainElement = document.createElement('main')
-const buttonElement = document.createElement('button')
 const imgElement = document.createElement('img')
+const buttonElement = document.createElement('button')
 const anchorElement = document.createElement('a')
 
 
 bodyElement.append(buttonElement)
-buttonElement.append('Beauty and the Beast')
-
-bodyElement.append(mainElement)
+buttonElement.append('Sunflowers and Roses')
 
 imgElement.className = 'image'
-imgElement.src = 'https://www.bitchmedia.org/sites/default/files/styles/article_page_featured_image/public/beauty-beast_032117_01.png?itok=5kpD8q-b'
+imgElement.src = 'https://static.wixstatic.com/media/f2123b_f03f26920e554034b1d1ffe51ce5c0da~mv2.png/v1/fill/w_930,h_930,al_c,q_90/f2123b_f03f26920e554034b1d1ffe51ce5c0da~mv2.webp'
 mainElement.append(imgElement)
 
-anchorElement.className = 'link'
-anchorElement.href = 'https://www.bitchmedia.org/sites/default/files/styles/article_page_featured_image/public/beauty-beast_032117_01.png?itok=5kpD8q-b'
+anchorElement.href = 'https://static.wixstatic.com/media/f2123b_f03f26920e554034b1d1ffe51ce5c0da~mv2.png/v1/fill/w_575,h_575,al_c,q_90,usm_0.66_1.00_0.01/f2123b_f03f26920e554034b1d1ffe51ce5c0da~mv2.webp'
 mainElement.append(anchorElement)
-anchorElement.append ('bitchMedia')
+anchorElement.append('FLOWERS by special arrangements')
+bodyElement.append(mainElement)
 
-buttonElement.addEventListener('click', function(){
-    mainElement.remove ()
+buttonElement.addEventListener('click', function() {
+mainElement.remove ()
 })
+
+
